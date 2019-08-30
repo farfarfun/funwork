ea04c7e8dbab6098b5ba3455a66115b5e332fb53
niuliangtao@youzan.com <1007530194@qq.com>
2019-08-30 15:53:26 +0800

    add

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 install_requires = []
 
 setup(name='notework',
-      version='0.0.15',
+      version='0.0.16',
       description='notework',
       author='euler',
       author_email='1007530194@qq.com',
